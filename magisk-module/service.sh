2e9e8c2301aa2c1e6e8b9c3ce009d79433436828
seclususs <antoa7767@gmail.com>
2026-01-09 05:11:26 +0700

    Harden daemon startup logic in service.sh

--- magisk-module/service.sh
+++ magisk-module/service.sh
@@ -1,4 +1,15 @@
 #!/system/bin/sh
 
 MODDIR="${0%/*}"
-$MODDIR/system/bin/qos_daemon &
+DAEMON_BIN="$MODDIR/system/bin/qos_daemon"
+DAEMON_NAME="qos_daemon"
+
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+  sleep 1
+done
+
+if pgrep -f "$DAEMON_NAME" > /dev/null; then
+  exit 0
+fi
+
+$DAEMON_BIN &
