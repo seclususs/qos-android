cbce8671f7c6795780071edc101280a16eea61f9
seclususs <antoa7767@gmail.com>
2026-01-22 00:18:33 +0700

    Migrate SurfaceFlinger calls to native bridge

--- core/src/hal/surface_flinger.rs
+++ core/src/hal/surface_flinger.rs
@@ -1,30 +1,15 @@
 //! Author: [Seclususs](https://github.com/seclususs)
 
+use crate::bindings::sys;
 use crate::daemon::types::QosError;
 
-use std::process::{Command, Stdio};
-
-const CMD_SERVICE: &str = "/system/bin/service";
-const SVC_SF: &str = "SurfaceFlinger";
-const TX_CODE: &str = "1035";
+use std::io;
 
 pub fn set_refresh_rate(param: i32) -> Result<(), QosError> {
-    let status = Command::new(CMD_SERVICE)
-        .arg("call")
-        .arg(SVC_SF)
-        .arg(TX_CODE)
-        .arg("i32")
-        .arg(param.to_string())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()
-        .map_err(|e| QosError::SystemCheckFailed(format!("Exec failed: {}", e)))?;
-    if status.success() {
-        Ok(())
+    let result = unsafe { sys::cpp_set_refresh_rate(param) };
+    if result < 0 {
+        Err(QosError::IoError(io::Error::last_os_error()))
     } else {
-        Err(QosError::SystemCheckFailed(format!(
-            "SF Transaction rejected. Exit code: {:?}",
-            status.code()
-        )))
+        Ok(())
     }
 }
