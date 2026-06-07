5168ced9b80840b74c3afcfe22f3d78e7139c7a7
seclususs <antoa7767@gmail.com>
2026-06-07 20:06:19 +0700

    Refine scripts to ensure complete leftover removal

--- magisk-module/common/apk/installer.sh
+++ magisk-module/common/apk/installer.sh
@@ -5,16 +5,16 @@ while [ "$(getprop sys.boot_completed)" != "1" ]; do
 done
 
 MOD_DIR="/data/adb/modules/sys_qos"
-APK_DIR="$MOD_DIR/common/apk"
-APK_PATH="$APK_DIR/com/seclususs/qos/com.seclususs.qos.apk"
+COMMON_DIR="$MOD_DIR/common"
+APK_PATH="$COMMON_DIR/apk/com/seclususs/qos/com.seclususs.qos.apk"
 PKG="com.seclususs.qos"
 
 if [ -f "$APK_PATH" ]; then
     pm install -r -d -g "$APK_PATH"
 fi
 
-if [ -d "$APK_DIR" ]; then
-    rm -rf "$APK_DIR"
+if [ -d "$COMMON_DIR" ]; then
+    rm -rf "$COMMON_DIR"
 fi
 
 rm -f "$0"
--- magisk-module/uninstall.sh
+++ magisk-module/uninstall.sh
@@ -2,8 +2,24 @@
 
 PKG="com.seclususs.qos"
 
-# Uninstall package
-pm uninstall "$PKG"
+# Force stop package
+am force-stop "$PKG" 2>/dev/null
 
-# Remove external storage data (Android/data)
-rm -rf /data/media/0/Android/data/$PKG
+# Uninstall package
+pm uninstall "$PKG" 2>/dev/null
+
+# Pause to allow system cleanup
+sleep 2
+
+# Remove app installation files
+rm -rf /data/app/*/*"$PKG"* 2>/dev/null
+rm -rf /data/app/"$PKG"* 2>/dev/null
+
+# Remove main app data & cache
+rm -rf /data/data/"$PKG" 2>/dev/null
+
+# Remove Device Encrypted storage data
+rm -rf /data/user_de/*/"$PKG" 2>/dev/null
+
+# Remove external storage data
+rm -rf /data/media/*/Android/data/"$PKG" 2>/dev/null
