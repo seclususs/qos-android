446af2f5bdeddbf1a889d509db85e8469d824b7c
seclususs <antoa7767@gmail.com>
2026-06-07 20:22:53 +0700

    Update for QoS v2.8 release

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.seclususs.qos"
         minSdk = 33
         targetSdk = 37
-        versionCode = 18
-        versionName = "2.7"
+        versionCode = 19
+        versionName = "2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
